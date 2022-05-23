7e4db341031e281888ae4f213b30778cb8a48ce6
John Anders <66134557+johnpatrickanders@users.noreply.github.com>
2022-05-23 08:13:47 -0400

    Update 98-Validate-Binary-Search-Tree.py

--- 98-Validate-Binary-Search-Tree.py
+++ 98-Validate-Binary-Search-Tree.py
@@ -17,3 +17,27 @@ class Solution:
                     valid(node.right, node.val, right))
         
         return valid(root, float("-inf"), float("inf"))
+
+    
+# PLEASE AVOID UNNECESSARY DOUBLE CALL:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        def validate(node, floor, ceil):
+            if not node:
+                return True
+            if node.val <= floor or node.val >= ceil:
+                return False
+            return validate(node.left, floor, node.val) and validate(node.right, node.val, ceil)
+        
+        return validate(root.left, float("-inf"), root.val) and validate(root.right, root.val, float("inf"))
